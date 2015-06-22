7844d963c8f8c7db97a3f7ddec7abd97ac7fb641
Jason E Anderson <ja@adadu.com>
2015-06-22 13:48:27 -0700

    band name gen w fav

--- app/script.js
+++ app/script.js
@@ -1,33 +1,59 @@
 "use strict";
 
+var adjective, verb, noun;
+
 $(function() {
   $("#name").on("click", function() {
     $.get("/adjective", function(response) {
-      var adjective = response.word;
+      adjective = response.word;
       $("#adjective").text(adjective);
     });
   });
 
   $("#name").on("click", function() {
     $.get("/verb", function(response) {
-      var verb = response.word;
+      verb = response.word;
       $("#verb").text(verb);
     });
   });
 
   $("#name").on("click", function() {
-      $.get("/noun", function(response) {
-        var noun = response.word;
-        $("#noun").text(noun);
-      });
+    $.get("/noun", function(response) {
+      noun = response.word;
+      $("#noun").text(noun);
     });
+  });
+
+// add fuctionality to favorites button
+  $("#fave-btn").on("click", function() {
+    var favorite = adjective + " " + verb + " " + noun;
+    var favePost;
+    // don't post band name if there isn't a favorite yet
+    // but do post response hint below
+    if (favorite === "undefined undefined undefined") {
+      favorite = null;
+      $.post("fave", favePost, function(response) {
+        var faveRes = response.msg;
+        $("#faveRes").text(faveRes);
+      });
+    }
+    // if there's a favorite then post
+    // and post response informing user of save
+    else if (favorite) {
+      $("#fave").text(favorite);
+      favePost = {word: favorite};
+      $.post("fave", favePost, function(response) {
+        var faveRes = response.msg;
+        $("#faveRes").text(faveRes);
+      });
+    }
+  });
+
 
   $("#submitWords").on("submit", function(e) {
     e.preventDefault();
     var adjective = $("input[name=adjective]").val();
     var adjPost;
-    //good practice to add if so it doesn't use server w/o
-    //text entered/submitted
     if (adjective) {
       adjPost = {word: adjective};
       $.post("adjective", adjPost, function(response) {
@@ -41,8 +67,6 @@ $(function() {
     e.preventDefault();
     var verb = $("input[name=verb]").val();
     var verbPost;
-    //good practice to add if so it doesn't use server w/o
-    //text entered/submitted
     if (verb) {
       verbPost = {word: verb};
       $.post("verb", verbPost, function(response) {
@@ -56,8 +80,6 @@ $(function() {
     e.preventDefault();
     var noun = $("input[name=noun]").val();
     var nounPost;
-    //good practice to add if so it doesn't use server w/o
-    //text entered/submitted
     if (noun) {
       nounPost = {word: noun};
       $.post("noun", nounPost, function(response) {
@@ -66,5 +88,4 @@ $(function() {
       });
     }
   });
-
 });
--- server.js
+++ server.js
@@ -1,62 +1,32 @@
 "use strict"
 
-/* There are two steps to this assignment.  If you need help, look at the slides
-and/or ask for help on Slack.
-
-
-
-First, make sure you have a working app from what we did in class. Here"s what
-you should have from class:
-
-A server that serves up an index.html file at the root url (/). That html file
-references two js files: jQuery, and your script.js file (or whatever it"s
-named). A button that, when clicked, sends a get request to a json endpoint.
-That endpoint returns a random adjective, which is then parsed and displayed in
-the browser.
-
-
-Once you have that working, do the following::
-
-Add two more objects in the server file: one for verbs, one for nouns. Follow
-the format we used for adjectives. Create get routes for these objects. Add two
-more Ajax get requests to your script.js file to retrieve the verbs and nouns.
-
-
-Write a couple sentences letting us know where you are with this. No need to
-turn in code for now. I advise getting this done on time, since this will (along
-with some other work) be a part of your final assignment. */
-
-// whatever express exports in json file we require and
-//assign to express var
 var express = require("express");
-//express function now is assigned to app
 var bodyparser = require("body-parser");
-//access .body or .auth to treat it like js from http-code
 var Adjective = require("./lib/adjective.js");
 var Noun = require("./lib/noun.js");
 var Verb = require("./lib/verb.js");
-// we have moved adjective to .lib folder
+// added favorite object with properties to be assigned
+var Favorite = require("./lib/favorite.js");
+// added post favorite method to save favorites
+var postFavorite = require("./lib/postFavorite.js");
 var getRandomWord = require("./lib/getRandomWord.js");
 var postRandomWord = require("./lib/postRandomWord.js");
+
 var app = express();
-//express module is framework that gives fetures for
-//web and mobile apps
+
 var port = process.env.PORT || 3000;
-//could make environment variable, if i don"t see that then go through port 3K
 
 app.use(bodyparser.json());
-//middlware between what one thing is doing and what your code is doing
 app.use(bodyparser.urlencoded({extended: true}));
-//makes it look pretty,
-
 app.use(express.static(__dirname + "/app/"));
-// this is middleware, now server knows to make server there
 
+// instantiate
 var adjective = new Adjective();
 var noun = new Noun();
 var verb = new Verb();
-// instantiate
-
+// create new Favorite object called favorite
+var favorite = new Favorite();
+// gets
 app.get("/", function(req, res) {
   res.sendFile("index.html");
 });
@@ -72,12 +42,12 @@ app.get("/verb", function(req, res) {
 app.get("/noun", function(req, res) {
   res.json(getRandomWord(noun));
 });
-
+//posts
 app.post("/adjective", function(req, res) {
   var word = postRandomWord(req.body.word, adjective);
-  res.json(word);
+  res.send(word);
 });
-//this root name is different than get root name
+
 app.post("/verb", function(req, res) {
   var word = postRandomWord(req.body.word, verb);
   res.json(word);
@@ -87,9 +57,13 @@ app.post("/noun", function(req, res) {
   var word = postRandomWord(req.body.word, noun);
   res.json(word);
 });
+// adding in post favorite method to check input/give user feedback and post
+// a favorite
+app.post("/fave", function(req, res) {
+  var word = postFavorite(req.body.word, favorite);
+  res.json(word);
+});
 
 app.listen(port, function() {
   console.log("server available at localhost: " + port);
 });
-
-// copy paste
