9f672c4ae3812c0abbd5263466ca62a72f504bdf
Jason E Anderson <ja@adadu.com>
2015-06-22 14:54:58 -0700

    fixed jscs erros, jshint erroring on $

--- app/script.js
+++ app/script.js
@@ -1,90 +1,91 @@
-"use strict";
+'use strict';
 
 var adjective, verb, noun;
 
 $(function() {
-  $("#name").on("click", function() {
-    $.get("/adjective", function(response) {
+  $('#name').on('click', function() {
+    $.get('/adjective', function(response) {
       adjective = response.word;
-      $("#adjective").text(adjective);
+      $('#adjective').text(adjective);
     });
   });
 
-  $("#name").on("click", function() {
-    $.get("/verb", function(response) {
+  $('#name').on('click', function() {
+    $.get('/verb', function(response) {
       verb = response.word;
-      $("#verb").text(verb);
+      $('#verb').text(verb);
     });
   });
 
-  $("#name").on("click", function() {
-    $.get("/noun", function(response) {
+  $('#name').on('click', function() {
+    $.get('/noun', function(response) {
       noun = response.word;
-      $("#noun").text(noun);
+      $('#noun').text(noun);
     });
   });
 
-// add fuctionality to favorites button
-  $("#fave-btn").on("click", function() {
-    var favorite = adjective + " " + verb + " " + noun;
+  // add fuctionality to favorites button
+  $('#fave-btn').on('click', function() {
+    var favorite = adjective + ' ' + verb + ' ' + noun;
     var favePost;
+
     // don't post band name if there isn't a favorite yet
     // but do post response hint below
-    if (favorite === "undefined undefined undefined") {
+    if (favorite === 'undefined undefined undefined') {
       favorite = null;
-      $.post("fave", favePost, function(response) {
+      $.post('fave', favePost, function(response) {
         var faveRes = response.msg;
-        $("#faveRes").text(faveRes);
+        $('#faveRes').text(faveRes);
       });
     }
+
     // if there's a favorite then post
     // and post response informing user of save
     else if (favorite) {
-      $("#fave").text(favorite);
+      $('#fave').text(favorite);
       favePost = {word: favorite};
-      $.post("fave", favePost, function(response) {
+      $.post('fave', favePost, function(response) {
         var faveRes = response.msg;
-        $("#faveRes").text(faveRes);
+        $('#faveRes').text(faveRes);
       });
     }
   });
 
-
-  $("#submitWords").on("submit", function(e) {
+  $('#submitWords').on('submit', function(e) {
     e.preventDefault();
-    var adjective = $("input[name=adjective]").val();
+    var adjective = $('input[name=adjective]').val();
     var adjPost;
     if (adjective) {
       adjPost = {word: adjective};
-      $.post("adjective", adjPost, function(response) {
+      $.post('adjective', adjPost, function(response) {
         var adjectiveRes = response.msg;
-        $("#adjectiveRes").text(adjectiveRes);
+        $('#adjectiveRes').text(adjectiveRes);
       });
     }
   });
 
-  $("#submitWords").on("submit", function(e) {
+  $('#submitWords').on('submit', function(e) {
     e.preventDefault();
-    var verb = $("input[name=verb]").val();
+    var verb = $('input[name=verb]').val();
     var verbPost;
     if (verb) {
       verbPost = {word: verb};
-      $.post("verb", verbPost, function(response) {
+      $.post('verb', verbPost, function(response) {
         var verbRes = response.msg;
-        $("#verbRes").text(verbRes);
+        $('#verbRes').text(verbRes);
       });
     }
   });
 
-  $("#submitWords").on("submit", function(e) {
+  $('#submitWords').on('submit', function(e) {
     e.preventDefault();
-    var noun = $("input[name=noun]").val();
+    var noun = $('input[name=noun]').val();
     var nounPost;
     if (noun) {
       nounPost = {word: noun};
-      $.post("noun", nounPost, function(response) {
+      $.post('noun', nounPost, function(response) {
         var nounRes = response.msg;
-        $("#nounRes").text(nounRes);
+        $('#nounRes').text(nounRes);
       });
     }
   });
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-"use strict"
+'use strict';
 
-var express = require("express");
-var bodyparser = require("body-parser");
-var Adjective = require("./lib/adjective.js");
-var Noun = require("./lib/noun.js");
-var Verb = require("./lib/verb.js");
+var express = require('express');
+var bodyparser = require('body-parser');
+var Adjective = require('./lib/adjective.js');
+var Noun = require('./lib/noun.js');
+var Verb = require('./lib/verb.js');
+
 // added favorite object with properties to be assigned
-var Favorite = require("./lib/favorite.js");
+var Favorite = require('./lib/favorite.js');
+
 // added post favorite method to save favorites
-var postFavorite = require("./lib/postFavorite.js");
-var getRandomWord = require("./lib/getRandomWord.js");
-var postRandomWord = require("./lib/postRandomWord.js");
+var postFavorite = require('./lib/postFavorite.js');
+var getRandomWord = require('./lib/getRandomWord.js');
+var postRandomWord = require('./lib/postRandomWord.js');
 
 var app = express();
 
@@ -18,52 +20,56 @@ var port = process.env.PORT || 3000;
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
-app.use(express.static(__dirname + "/app/"));
+app.use(express.static(__dirname + '/app/'));
 
 // instantiate
 var adjective = new Adjective();
 var noun = new Noun();
 var verb = new Verb();
+
 // create new Favorite object called favorite
 var favorite = new Favorite();
+
 // gets
-app.get("/", function(req, res) {
-  res.sendFile("index.html");
+app.get('/', function(req, res) {
+  res.sendFile('index.html');
 });
 
-app.get("/adjective", function(req, res) {
+app.get('/adjective', function(req, res) {
   res.json(getRandomWord(adjective));
 });
 
-app.get("/verb", function(req, res) {
+app.get('/verb', function(req, res) {
   res.json(getRandomWord(verb));
 });
 
-app.get("/noun", function(req, res) {
+app.get('/noun', function(req, res) {
   res.json(getRandomWord(noun));
 });
+
 //posts
-app.post("/adjective", function(req, res) {
+app.post('/adjective', function(req, res) {
   var word = postRandomWord(req.body.word, adjective);
   res.send(word);
 });
 
-app.post("/verb", function(req, res) {
+app.post('/verb', function(req, res) {
   var word = postRandomWord(req.body.word, verb);
   res.json(word);
 });
 
-app.post("/noun", function(req, res) {
+app.post('/noun', function(req, res) {
   var word = postRandomWord(req.body.word, noun);
   res.json(word);
 });
+
 // adding in post favorite method to check input/give user feedback and post
 // a favorite
-app.post("/fave", function(req, res) {
+app.post('/fave', function(req, res) {
   var word = postFavorite(req.body.word, favorite);
   res.json(word);
 });
 
 app.listen(port, function() {
-  console.log("server available at localhost: " + port);
+  console.log('server available at localhost: ' + port);
 });
